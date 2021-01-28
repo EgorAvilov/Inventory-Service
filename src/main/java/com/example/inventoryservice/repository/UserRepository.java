dc4041db52839622dec8091156b2013c485ddc5f
Kyrsor <egoraviov99@mail.ru>
2021-01-28 21:49:46 +0300

    Updated UserServiceImpl

--- src/main/java/com/example/inventoryservice/repository/UserRepository.java
+++ src/main/java/com/example/inventoryservice/repository/UserRepository.java
@@ -4,6 +4,13 @@ import com.example.inventoryservice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    List<User> findAllByUsernameIgnoreCase(String username);
+
+    List<User> findAllByRestaurant_Id(Long restaurantId);
 }
--- src/main/java/com/example/inventoryservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/inventoryservice/service/impl/UserServiceImpl.java
@@ -1,35 +1,69 @@
 package com.example.inventoryservice.service.impl;
 
+import com.example.inventoryservice.converter.UserConverter;
 import com.example.inventoryservice.dto.UserDto;
+import com.example.inventoryservice.entity.User;
+import com.example.inventoryservice.repository.UserRepository;
 import com.example.inventoryservice.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
-    @Override
-    public UserDto findByUsername(String username) {
-        return null;
+    private final UserRepository userRepository;
+    private final UserConverter userConverter;
+    Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    public UserServiceImpl(UserRepository userRepository, UserConverter userConverter) {
+        this.userRepository = userRepository;
+        this.userConverter = userConverter;
     }
 
     @Override
-    public UserDto getCurrentUser() {
-        return null;
+    public UserDto findByUsername(String username) {
+        logger.info("Find user by username {}", username);
+        User user = userRepository.findByUsername(username);
+        return userConverter.entityToDto(user);
     }
+
+    @Override
+    @Transactional
+    public UserDto getCurrentUser() {
+        logger.info("Get current user");
+        Authentication authentication = SecurityContextHolder.getContext()
+                                                             .getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        return findByUsername(currentPrincipalName);
+    }
+
 
     @Override
     public boolean usernameExists(String username) {
-        return false;
+        logger.info("Check for existing user username {}", username);
+        return userRepository.findAllByUsernameIgnoreCase(username)
+                             .size() != 0;
     }
 
     @Override
     public UserDto create(UserDto userDto) {
-        return null;
+        logger.info("Create user");
+        User user = userConverter.dtoToEntity(userDto);
+        User persistUser = userRepository.save(user);
+        return userConverter.entityToDto(persistUser);
     }
 
     @Override
     public List<UserDto> findAllByRestaurant() {
-        return null;
+        logger.info("Find all users by restaurant");
+        UserDto userDto = getCurrentUser();
+        List<User> users = userRepository.findAllByRestaurant_Id(userDto.getRestaurant()
+                                                                        .getId());
+        return userConverter.entityToDto(users);
     }
 }
