de75eec50841712fefe5f9edcd2a739cb2390d44
Kyrsor <egoraviov99@mail.ru>
2021-01-28 21:32:27 +0300

    Updated dto

--- src/main/java/com/example/inventoryservice/dto/DishDto.java
+++ src/main/java/com/example/inventoryservice/dto/DishDto.java
@@ -1,4 +1,23 @@
 package com.example.inventoryservice.dto;
 
+import com.example.inventoryservice.entity.Restaurant;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class DishDto {
+
+    private Long id;
+
+    private String name;
+
+    private Restaurant restaurant;
+
+    private BigDecimal price;
 }
